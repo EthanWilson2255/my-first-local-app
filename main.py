1fb04b9f7977604d2115fa05ee52d24f63a8bd05
Ethan Wilson <eswilson@seu.edu>
2026-04-28 00:00:34 -0400

    Startup UI

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import tkinter as tk
 
 from tkinter import messagebox
 
-class MyGUI:
+class BudgetApp:
 
     def __init__(self):
 
         self.root = tk.Tk()
+
+        self.root.title("World's Best Budgeting App")
 
         self.menubar = tk.Menu(self.root)
 
@@ -23,43 +25,40 @@ class MyGUI:
 
         self.root.config(menu=self.menubar)
 
-        self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18))
+        self.label = tk.Label(self.root, text="Welcome to the World's Best Budgeting App!", font=('Arial', 18))
         self.label.pack(padx=10, pady=10)
 
-        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
-        self.textbox.bind("<KeyPress>", self.shortcut)
-        self.textbox.pack(padx=10, pady=10)
+        self.label2 = tk.Label(self.root, text="Please enter your name Below!")
+        self.label2.pack(padx=10, pady=10)
 
-        self.check_state = tk.IntVar()
+        self.name_entry = tk.Entry()
+        self.name_entry.pack(padx=10, pady=10)
 
-        self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 16), variable=self.check_state)
-        self.check.pack(padx=10, pady=10)
-
-        self.button = tk.Button(self.root, text="Show Message", font=('Arial', 18), command=self.show_message)
+        self.button = tk.Button(self.root, text="Ready to Budget? Click Me!!!", font=('Arial', 18), command=self.enterforum)
         self.button.pack(padx=10, pady=10)
-
-        self.clearbtn = tk.Button(self.root, text="Clear", font=('Arial', 18), command=self.clear)
-        self.clearbtn.pack(padx=10, pady=10)
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
     
-    def show_message(self):
-        if self.check_state.get() == 0:
-            print(self.textbox.get('1.0', tk.END))
+    def enterforum(self):
+        if self.name_entry.get() == "":
+            self.show_message()
         else:
-            messagebox.showinfo(title="Message", message=self.textbox.get('1.0', tk.END))
+            self.root2 = tk.Tk()
 
-    def shortcut(self, event):
-       if event.state == 8 and event.keysym == "Return":
-           self.show_message()
+            self.root2.title(f"{self.name_entry.get()}'s Budget!")
+
+            self.budgetlabel = tk.Label(self.root2, text=f"{self.name_entry.get()}", font=('Arial', 18))
+            self.budgetlabel.pack(padx=10, pady=10)
+
+            self.root2.mainloop()
 
     def on_closing(self):
         if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
             self.root.destroy()
 
-    def clear(self):
-        self.textbox.delete('1.0', tk.END)
+    def show_message(self): 
+        messagebox.showinfo(title="Message", message="Please enter your name first!")
         
  
-MyGUI()
+BudgetApp()
